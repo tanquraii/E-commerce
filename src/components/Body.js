2f5fa616da4075dc4fca118cddb0fc4f41022557
tanqurai <higashigatajosuke16@gmail.com>
2024-04-21 00:33:49 +0600

    added image changer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import BigShoe from './images/image-product-1-thumbnail.jpg'
-import LittleShoe1 from './images/image-product-2-thumbnail.jpg'
-import LittleShoe2 from './images/image-product-3-thumbnail.jpg'
-import LittleShoe3 from './images/image-product-4-thumbnail.jpg'
+import LittleShoe1 from './images/image-product-2.jpg'
+import LittleShoe2 from './images/image-product-3.jpg'
+import LittleShoe3 from './images/image-product-4.jpg'
 import VeryBigShoe from './images/image-product-1.jpg'
 import Buttons from './Buttons'
 import { useState } from 'react'
+import { useEffect } from 'react'
 const Body = () => {
   const [image,setImage] = useState(VeryBigShoe);
-  function changeImage(kartinka){
+  const changeImage = (kartinka)=>{
     setImage(kartinka);
   }
+  useEffect(()=>{
+    changeImage(image)
+  },[])
   return (
     <body>
       <div className='container'>
@@ -19,10 +22,10 @@ const Body = () => {
             <img src={image} alt="bigshoe" />
           </div>
           <div className="bottom">
-            <img onClick = {changeImage(BigShoe)} src={BigShoe} alt="bigshoe" />
-            <img onClick = {changeImage(LittleShoe1)} src={LittleShoe1} alt="lilshoe" />
-            <img src={LittleShoe2} alt="lilshoe" />
-            <img src={LittleShoe3} alt="lilshoe" />
+            <img onClick = {()=>changeImage(VeryBigShoe)} src={VeryBigShoe} alt="bigshoe" />
+            <img onClick = {()=>changeImage(LittleShoe1)} src={LittleShoe1} alt="lilshoe" />
+            <img onClick = {()=>changeImage(LittleShoe2)} src={LittleShoe2} alt="lilshoe" />
+            <img onClick = {()=>changeImage(LittleShoe3)}  src={LittleShoe3} alt="lilshoe" />
           </div>
         </span>
         
